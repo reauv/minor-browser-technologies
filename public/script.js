cb3366ce18b31bfed48f77cb787e507b7678b33b
reauv <studio@rovansteen.nl>
2016-03-24 16:12:42 +0100

    Add websocket implementation

--- public/script.js
+++ public/script.js
@@ -1,14 +1,44 @@
 (function() {
-    var results = document.querySelector('#results');
+    var resultsContainer = document.querySelector('#results');
+    var formContainer = document.querySelector('#form');
     var content = document.querySelector('#content');
     var voteButton = document.querySelector('#vote');
+    var WEBSOCKET_HOST = 'ws://145.28.120.199:8888';
 
-    if(results) {
-        setInterval(fetchResults, 6000);
+    function updateResults(results) {
+        resultsContainer.innerHTML = '';
+        results.forEach(function(result) {
+            var el = document.createElement('div');
+            el.innerHTML = result.label + ' - ' + result.votes + ' stemmen'
+            resultsContainer.appendChild(el);
+        });
+    };
+
+    function hideForm() {
+        formContainer.style.display = 'none';
     }
 
-    if(voteButton) {
-        voteButton.addEventListener('click', function(event) {
+    function showResults() {
+        resultsContainer.style.display = 'block';
+    }
+
+    ajax = {
+        fetchResults: function() {
+            var request = new XMLHttpRequest();
+            request.open('GET', '/api/results', true);
+            request.send();
+
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400) {
+                    var data = JSON.parse(request.responseText);
+                    hideForm();
+                    showResults();
+                    updateResults(data);
+                }
+            }
+        },
+
+        onVoteButtonClick: function() {
             var answer = document.querySelector('input[name="answer"]:checked');
             var value = answer.value;
 
@@ -18,24 +48,95 @@
 
             request.onload = function () {
                 if (request.status >= 200 && request.status < 400) {
-                    fetchResults();
-                    setInterval(fetchResults, 6000);
+                    this.fetchResults();
+                    setInterval(this.fetchResults, 6000);
                 }
-            }
+            }.bind(this);
 
             event.preventDefault();
-        });
-    }
+        },
 
-    function fetchResults() {
-        var request = new XMLHttpRequest();
-        request.open('GET', '/api/html/results', true);
-        request.send();
+        init: function() {
+            console.log('Using AJAX');
 
-        request.onload = function() {
-            if (request.status >= 200 && request.status < 400) {
-                content.innerHTML = request.responseText;
+            if(voteButton) {
+                voteButton.addEventListener('click', this.onVoteButtonClick.bind(this));
             }
         }
+    },
+
+    websockets = {
+        connection: null,
+
+        setupConnection: function() {
+            var id = Cookies.get('id');
+            this.connection = new WebSocket(WEBSOCKET_HOST + '?id=' + id, 'echo-protocol');
+
+            this.connection.onopen = function() {
+                console.log('Connection open');
+            }
+
+            this.connection.onmessage = function(message) {
+                var data = JSON.parse(message.data);
+                console.log('Message received:');
+                console.log(data);
+
+                if (data.type === 'CONNECTED') {
+                    this.onConnected(data);
+                }
+
+                if (data.type === 'VOTE_PROCESSED') {
+                    this.onVoteProcessed();
+                }
+
+                if (data.type === 'NEW_VOTE') {
+                    this.onNewVote(data);
+                }
+            }.bind(this);
+
+            this.connection.onclose = function() {
+                console.log('Connection closed');
+            }
+        },
+
+        onVoteButtonClick: function() {
+            var answer = document.querySelector('input[name="answer"]:checked');
+            var value = answer.value;
+
+            event.preventDefault();
+            this.connection.send(JSON.stringify({ type: 'vote', value: value }));
+        },
+
+        onConnected: function(data) {
+            Cookies.set('id', data.payload.id);
+        },
+
+        onVoteProcessed: function() {
+            hideForm();
+            showResults();
+        },
+
+        onNewVote: function(data) {
+            updateResults(data.payload.votesByAnswers);
+        },
+
+        init: function() {
+            console.log('Using websockets');
+            this.setupConnection();
+
+            if(voteButton) {
+                voteButton.addEventListener('click', this.onVoteButtonClick.bind(this));
+            }
+        }
+    }
+
+    if (!document.query)
+
+    if('WebSocket' in window || 'MozWebSocket' in window) {
+        document.querySelector('html').classList.add('js');
+        websockets.init();
+    } else {
+        document.querySelector('html').classList.add('js');
+        ajax.init();
     }
 }());
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+var fs = require('fs');
 var path = require('path');
+var http = require('http');
 var express = require('express');
 var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var WebSocketServer = require('websocket').server;
 
-var server = express();
+var app = express();
+var server = http.createServer(app);
 
-server.set('view engine', 'hbs');
-server.use(session({secret: 'secret'}));
-server.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 var answers = [
     { id: '1', label: 'Krijn' },
@@ -45,21 +51,56 @@ function getUserVote(sessionId) {
     });
 }
 
-server.get('/', function (request, response) {
-    if(request.query.answer && !request.session.voted) {
-        request.session.voted = true;
+function generateClientKey() {
+    return Math.random().toString(36).substring(7);
+}
+
+function findClientByConnection(connection) {
+    return clients.find(function(client) {
+        return client.connection === connection;
+    });
+}
+
+function findClientById(id) {
+    if(!id || id.length < 1) {
+        return null;
+    }
+
+    return clients.find(function(client) {
+        return client.id === id;
+    });
+}
+
+function getClient(id) {
+    var client = findClientById(id);
+
+    if(client) {
+        return client;
+    } else {
+        client = { id: generateClientKey(), connection: null, voted: false }
+        clients.push(client);
+        return client;
+    }
+}
+
+/**
+ * HTTP server
+ */
+app.get('/', function (request, response) {
+    var client = getClient(request.cookies.id);
+
+    if(request.query.answer && !client.voted) {
+        client.voted = true;
         votes.push({
-            session: request.session.id,
             answer: request.query.answer,
         });
     }
 
-    if(request.session.voted) {
+    if(client.voted) {
         return response.render(
             'results',
             {
                 votesByAnswers: getVotesByAnswers(),
-                userVote: getUserVote(request.session.id)
             }
         )
     }
@@ -70,11 +111,12 @@ server.get('/', function (request, response) {
 /**
  * JSON API
  */
-server.get('/api/vote/:id', function (request, response) {
-    if(request.params.id && !request.session.voted) {
-        request.session.voted = true;
+app.get('/api/vote/:id', function (request, response) {
+    var client = getClient(request.cookies.id);
+
+    if(request.params.id && !client.voted) {
+        client.voted = true;
         votes.push({
-            session: request.session.id,
             answer: request.params.id,
         });
 
@@ -85,10 +127,14 @@ server.get('/api/vote/:id', function (request, response) {
     response.end('You are not allowed to cast a vote');
 });
 
+app.get('/api/results', function (request, response) {
+    return response.json(getVotesByAnswers());
+});
+
 /**
  * HTML API
  */
-server.get('/api/html/results', function (request, response) {
+app.get('/api/html/results', function (request, response) {
     return response.render(
         'results',
         {
@@ -99,5 +145,106 @@ server.get('/api/html/results', function (request, response) {
     )
 });
 
-console.log('Listening at localhost on port 8080');
-server.listen('8080');
+server.listen(8080, function() {
+    console.log((new Date()) + ' HTTP Server is listening on port 8080');
+});
+
+/**
+ * Websockets
+ */
+var clients = [];
+
+var wsHttpServer = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+wsServer = new WebSocketServer({
+    httpServer: wsHttpServer,
+    autoAcceptConnections: false,
+ });
+
+function originIsAllowed(origin) {
+    return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+        request.reject();
+        return console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    }
+
+    var id = request.resourceURL.query.id;
+
+    var connection = request.accept('echo-protocol', request.origin);
+    var client = getClient(id);
+    client.connection = connection;
+
+    wsOutput.connected({ id: client.id }, connection);
+
+    connection.on('message', function(message) {
+        console.log('Message received from ' + client.id);
+        var payload = JSON.parse(message.utf8Data);
+
+        if (payload.type === 'vote') {
+            wsInput.onVote(payload, connection);
+        }
+    });
+
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
+
+wsInput = {
+    onVote: function(payload, connection) {
+        votes.push({
+            answer: payload.value,
+        });
+
+        var client = findClientByConnection(connection);
+        client.voted = true;
+
+        wsOutput.voteProcessed(payload, connection);
+        wsOutput.newVote(payload, connection);
+    }
+}
+
+wsOutput = {
+    connected: function(payload, connection) {
+        var message = {
+            type: 'CONNECTED',
+            payload: payload,
+        }
+
+        connection.send(JSON.stringify(message));
+    },
+
+    voteProcessed: function(payload, connection) {
+        var message = {
+            type: 'VOTE_PROCESSED',
+        }
+
+        connection.send(JSON.stringify(message));
+    },
+
+    newVote: function(payload, connection) {
+        var message = {
+            type: 'NEW_VOTE',
+            payload: {
+                votesByAnswers: getVotesByAnswers(),
+            },
+        }
+
+        clients.forEach(function(client) {
+            if (client.voted) {
+                client.connection.send(JSON.stringify(message));
+            }
+        });
+    }
+}
+
+wsHttpServer.listen(8888, function() {
+    console.log((new Date()) + ' Websocket Server is listening on port 8888');
+});
