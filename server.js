a3eec714102f13e76ccf4bac5f1ae3a90f2b0aec
Robert van Steen <studio@rovansteen.nl>
2016-05-16 11:08:34 +0200

    Add http-equiv refresh to auto refresh without JS

--- server.js
+++ server.js
@@ -15,6 +15,24 @@ var server = http.createServer(app);
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
+var blocks = {};
+
+hbs.registerHelper('extend', function(name, context) {
+    var block = blocks[name];
+    if (!block) {
+        block = blocks[name] = [];
+    }
+
+    block.push(context.fn(this));
+});
+
+hbs.registerHelper('block', function(name) {
+    var val = (blocks[name] || []).join('\n');
+
+    blocks[name] = [];
+    return val;
+});
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
